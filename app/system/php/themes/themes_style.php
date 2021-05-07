4287d8a399e8b895b4888b8bd25ff258699275ab
Paulo <cezarleigos28@outlook.com>
2021-05-07 16:40:54 -0300

    v0.03 - Creating the theme flow
    This time we started to create a flow of routes so that the user can choose the color of the system panel.

---
+++ app/system/php/themes/themes_style.php
@@ -0,0 +1,42 @@
+<?php
+
+    $theme_name = "theme_basic";
+    $theme_text_color = "";
+    $theme_background_color = "";
+    $theme_border_color = "";
+
+    if($theme_name == "theme_basic"){
+        $theme_text_color = "";
+        $theme_background_color = "";
+        $theme_border_color = "";
+    }elseif($theme_name == "theme_blue"){
+        $theme_text_color = "";
+        $theme_background_color = "";
+        $theme_border_color = "";
+    }elseif($theme_name == "theme_red"){
+    	$theme_text_color = "";
+        $theme_background_color = "";
+        $theme_border_color = "";
+    }elseif($theme_name == "theme_yellow"){
+    	$theme_text_color = "";
+        $theme_background_color = "";
+        $theme_border_color = "";
+    }elseif($theme_name == "theme_green"){
+    	$theme_text_color = "";
+        $theme_background_color = "";
+        $theme_border_color = "";
+    }elseif($theme_name == "theme_black"){
+    	$theme_text_color = "";
+        $theme_background_color = "";
+        $theme_border_color = "";
+    }elseif($theme_name == "theme_purple"){
+    	$theme_text_color = "";
+        $theme_background_color = "";
+        $theme_border_color = "";
+    }else{
+        $theme_text_color = "";
+        $theme_background_color = "";
+        $theme_border_color = "";
+    }
+    
+?>
