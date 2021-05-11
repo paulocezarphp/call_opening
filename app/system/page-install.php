1582a3695ff652609704b8c72093d048b80ab331
Cezar Leigos <cezarleigos28@outlook.com>
2021-05-11 20:02:53 -0300

    v0.07 - Starting to create installer
    This time we started to create the page responsible for installing the system on the user's server, we also created the css style sheet to create the installation panel.

---
+++ app/system/page-install.php
@@ -0,0 +1,54 @@
+<div class="install">
+   <div class="install-group">
+   	   <img src="" class="install-image">
+   	   <div>
+   	      <div class="install-title">Call Opening</div>
+   	      <div class="install-description">Complete all fields correctly</div>	
+   	   </div>
+   	   <div class="install-group-div">
+   	   	  <div class="install-group-left">
+   	   	  	  <div class="install-group-label">Label</div>
+   	   	  	  <input type="text" name="" class="install-group-input" placeholder="" autocomplete="off">
+   	   	  	  <div class="install-group-label">Label</div>
+   	   	  	  <input type="text" name="" class="install-group-input" placeholder="" autocomplete="off">
+   	   	  	  <div class="install-group-label">Label</div>
+   	   	  	  <input type="text" name="" class="install-group-input" placeholder="" autocomplete="off">
+   	   	  	  <div class="install-group-label">Label</div>
+   	   	  	  <input type="text" name="" class="install-group-input" placeholder="" autocomplete="off">
+   	   	  	  <div class="install-group-label">Label</div>
+   	   	  	  <input type="text" name="" class="install-group-input" placeholder="" autocomplete="off">
+   	   	  	  <div class="install-group-label">Label</div>
+   	   	  	  <input type="text" name="" class="install-group-input" placeholder="" autocomplete="off">
+   	   	  	  <div class="install-group-label">Label</div>
+   	   	  	  <input type="text" name="" class="install-group-input" placeholder="" autocomplete="off">
+   	   	  	  <div class="install-group-label">Label</div>
+   	   	  	  <input type="text" name="" class="install-group-input" placeholder="" autocomplete="off">
+   	   	  	  <div class="install-group-label">Label</div>
+   	   	  	  <input type="text" name="" class="install-group-input" placeholder="" autocomplete="off">
+   	      </div>
+   	      <div class="install-group-right">
+   	      	  <div class="install-group-label">Label</div>
+   	   	  	  <input type="text" name="" class="install-group-input" placeholder="" autocomplete="off">
+   	   	  	  <div class="install-group-label">Label</div>
+   	   	  	  <input type="text" name="" class="install-group-input" placeholder="" autocomplete="off">
+   	   	  	  <div class="install-group-label">Label</div>
+   	   	  	  <input type="text" name="" class="install-group-input" placeholder="" autocomplete="off">
+   	   	  	  <div class="install-group-label">Label</div>
+   	   	  	  <input type="text" name="" class="install-group-input" placeholder="" autocomplete="off">
+   	   	  	  <div class="install-group-label">Label</div>
+   	   	  	  <input type="text" name="" class="install-group-input" placeholder="" autocomplete="off">
+   	   	  	  <div class="install-group-label">Label</div>
+   	   	  	  <input type="text" name="" class="install-group-input" placeholder="" autocomplete="off">
+   	   	  	  <div class="install-group-label">Label</div>
+   	   	  	  <input type="text" name="" class="install-group-input" placeholder="" autocomplete="off">
+   	   	  	  <div class="install-group-label">Label</div>
+   	   	  	  <input type="text" name="" class="install-group-input" placeholder="" autocomplete="off">
+   	   	  	  <div class="install-group-label">Label</div>
+   	   	  	  <input type="text" name="" class="install-group-input" placeholder="" autocomplete="off">
+   	      </div>
+   	   </div>
+   	   <div>
+   	   	  <button type="submit" class="install-submit">Finish</button>
+   	   </div>	
+   </div>	
+</div>	
