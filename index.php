0ea553a8ff7556d68207b810cb5b8c05fab70426
Paulo <cezarleigos28@outlook.com>
2021-09-06 09:05:04 -0300

    v0.09 - Creating files
    This time we started to create some more files so that our system has the necessary resources so that we can develop it quickly.

--- index.php
+++ index.php
@@ -20,6 +20,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <link rel="sortcut icon" href="app/system/images/icon.png" type="image/x-icon" />
+  <link rel="stylesheet" type="text/css" href="app/system/css/style-mobile.css">
+  <link rel="stylesheet" type="text/css" href="app/system/css/style-tablet.css">
+  <link rel="stylesheet" type="text/css" href="app/system/css/style-desktop.css">
 </head>
 <body>
    
