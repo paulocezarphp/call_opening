ad01ea313cd9184de269123e0268449cd61c5220
Paulo <cezarleigos28@outlook.com>
2021-09-23 09:40:29 -0300

    v0.10 - Creating modules
    This time we start to create the modules that will be responsible for making the operations and manipulation of the system as a whole, we will create a module to be responsible for each operation, this will help us make each of the modules independent of each other, facilitating future maintenance .

--- app.php
+++ app.php
@@ -1,4 +1,45 @@
 <?php
 
 
+@$operation = $_POST['app'];
+
+if($operation == "validate-login"){
+
+   	//Operation...
+
+}elseif($operation == "validate-quit-login"){
+
+   	//Operation...
+
+}elseif($operation == "?"){
+
+    //Operation...	
+
+}elseif($operation == "??"){
+
+    //Operation...	
+
+}elseif($operation == "???"){
+
+    //Operation...	
+
+}elseif($operation == "????"){
+
+    //Operation...	
+
+}elseif($operation == "?????"){
+
+    //Operation...	
+
+}elseif($operation == "??????"){
+
+    //Operation...	
+
+}else{
+
+	header("Location: ./");
+
+}
+
+
 ?>
